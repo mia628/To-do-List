063e5f7f8b3215ad0d0799cc528bd55bf9426f1c
mia628 <mia.1611628@gmail.com>
2024-07-04 01:48:48 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,78 @@
+// 유저가 값을 입력한다
+// + 버튼을 클릭하면, 할일이 추가된다
+// delete 버튼을 누르면 할일이 삭제된다
+// check 버튼을 누르면 할일이 끝나면서 밑줄이 간다
+// 1. check 버튼을 클릭하는 순간 false => true
+// 2. true이면 끝난걸로 간주하고 밑줄 보여주기
+// 3. false이면 안끝난걸로 간주하고 그대로
+
+// 진행중 끝남 탭을 누르면, 언더바가 이동한다
+// 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만
+// 전체탭을 누르면 다시 전체아이템으로 돌아옴
+
+let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
 let taskList = [];
-let taskInput = document.getElementById("task-input");
-let checkButton = document.getElementById("check-button");
-let deleteButton = document.getElementById("delete-button");
 
 addButton.addEventListener("click", addTask);
-// checkButton.addEventListener("click", check);
-// deleteButton.addEventListener("click", delet);
 
-
-
-function addTask(){
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
-    console.log(taskList);
-    render()
+function addTask() {
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
+  console.log(taskList);
+  render();
 }
 
-function render(){
-    let resultHTML = '';
-    for(let i=0 ; i<taskList.length ; i++){
-        resultHTML += `<div class="task">
-                    <div>${taskList[i]}</div>   
-                    <div>
-                        <button id="check-button">Check</button>
-                        <button id="delete-button">Delete</button>
+function render() {
+  let resultHTML = "";
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].isComplete == true) {
+      resultHTML += `<div class="task" style="background-color: lightgray;">
+                    <div class="task-done" style="padding: 1em;">${taskList[i].taskContent}</div>
+                    <div class="icons">
+                        <img class="icon" src="images/return.png" onclick="toggleComplete('${taskList[i].id}')"><span class="gap"></span>
+                        <img class="icon" src="images/delete.png" onclick="deleteTask('${taskList[i].id}')">
                     </div>
-                </div>`
+                </div>`;
+    } else {
+      resultHTML += `<div class="task">
+            <div style="padding: 1em;">${taskList[i].taskContent}</div>
+            <div class="icons">
+                <img class="icon" src="images/check.png" onclick="toggleComplete('${taskList[i].id}')"><span class="gap"></span>
+                <img class="icon" src="images/delete.png" onclick="deleteTask('${taskList[i].id}')">
+            </div>
+        </div>`;
     }
+  }
 
-    document.getElementById("task-board").innerHTML = resultHTML;
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+  console.log(taskList);
+}
+
+function deleteTask(id) {
+    for(let i = 0; i < taskList.length; i++) {
+        if(taskList[i].id == id) {
+            taskList.splice(i, 1);
+            break;
+        }
+    }
+    render();
+}
+
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
