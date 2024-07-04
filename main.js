dc71b413303c9901fd3b8a642194740fcfd62e68
mia628 <mia.1611628@gmail.com>
2024-07-05 01:41:09 +0900

    third commit

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
-// 유저가 값을 입력한다
-// + 버튼을 클릭하면, 할일이 추가된다
-// delete 버튼을 누르면 할일이 삭제된다
-// check 버튼을 누르면 할일이 끝나면서 밑줄이 간다
-// 1. check 버튼을 클릭하는 순간 false => true
-// 2. true이면 끝난걸로 간주하고 밑줄 보여주기
-// 3. false이면 안끝난걸로 간주하고 그대로
-
-// 진행중 끝남 탭을 누르면, 언더바가 이동한다
-// 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만
-// 전체탭을 누르면 다시 전체아이템으로 돌아옴
-
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = [];
+let mode = "all";
+let filterList = [];
+let underLine = document.getElementById("under-line");
+let horizontalTabs = document.querySelectorAll(".task-tabs div:not(#under-line)");
+
+document.addEventListener("DOMContentLoaded", function() {
+  let firstTab = document.getElementById("all");
+  underLine.style.left = firstTab.offsetLeft + "px";
+  underLine.style.width = firstTab.offsetWidth + "px";
+  underLine.style.bottom = "0px";
+});
+
+taskInput.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
 
 addButton.addEventListener("click", addTask);
 
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  })
+}
+
 function addTask() {
+  if (taskInput.value == "") {
+    alert("할 일을 입력해 주세요.");
+    return;
+  }
   let task = {
     id: randomIDGenerate(),
     taskContent: taskInput.value,
@@ -24,26 +40,36 @@ function addTask() {
   };
   taskList.push(task);
   console.log(taskList);
+  taskInput.value = "";
   render();
 }
 
 function render() {
+  // 1. 내가 선택한 탭에 따라서
+  let list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "ongoing" || mode === "done") {
+    list = filterList;
+  }
+
+  // 2. 리스트를 달리 보여준다
   let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete == true) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
       resultHTML += `<div class="task" style="background-color: lightgray;">
-                    <div class="task-done" style="padding: 1em;">${taskList[i].taskContent}</div>
+                    <div class="task-done" style="padding: 1em;">${list[i].taskContent}</div>
                     <div class="icons">
-                        <img class="icon" src="images/return.png" onclick="toggleComplete('${taskList[i].id}')"><span class="gap"></span>
-                        <img class="icon" src="images/delete.png" onclick="deleteTask('${taskList[i].id}')">
+                        <img class="icon" src="images/return.png" onclick="toggleComplete('${list[i].id}')"><span class="gap"></span>
+                        <img class="icon" src="images/delete.png" onclick="deleteTask('${list[i].id}')">
                     </div>
                 </div>`;
     } else {
       resultHTML += `<div class="task">
-            <div style="padding: 1em;">${taskList[i].taskContent}</div>
+            <div style="padding: 1em;">${list[i].taskContent}</div>
             <div class="icons">
-                <img class="icon" src="images/check.png" onclick="toggleComplete('${taskList[i].id}')"><span class="gap"></span>
-                <img class="icon" src="images/delete.png" onclick="deleteTask('${taskList[i].id}')">
+                <img class="icon" src="images/check.png" onclick="toggleComplete('${list[i].id}')"><span class="gap"></span>
+                <img class="icon" src="images/delete.png" onclick="deleteTask('${list[i].id}')">
             </div>
         </div>`;
     }
@@ -70,9 +96,54 @@ function deleteTask(id) {
             break;
         }
     }
+
+    if (mode === "ongoing" || mode === "done") {
+      filterList = taskList.filter(task => {
+          if (mode === "ongoing") {
+              return !task.isComplete;
+          } else if (mode === "done") {
+              return task.isComplete;
+          }
+      });
+  }
+  
+  render();
+}
+
+function filter(event) {
+  mode = event.target.id;
+  filterList = [];
+  if (mode === "all") {
     render();
+  } else if (mode === "ongoing") {
+    // 진행중인 아이템을 보여준다
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  } else if(mode === "done") {
+    // 끝나는 케이스
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
 }
 
 function randomIDGenerate() {
   return "_" + Math.random().toString(36).substr(2, 9);
+}
+
+horizontalTabs.forEach((menu) =>
+  menu.addEventListener("click", (e) => horizontalIndicator(e))
+);
+
+function horizontalIndicator(e) {
+  underLine.style.left = e.currentTarget.offsetLeft + "px";
+  underLine.style.width = e.currentTarget.offsetWidth + "px";
+  underLine.style.bottom = "0px";
 }
